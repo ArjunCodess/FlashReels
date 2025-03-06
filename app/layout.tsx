1ee2d704d9f372faeb8ba57bdd8f84e658a4a753
Arjun Vijay Prakash <arjunv.prakash12345@gmail.com>
2025-03-06 15:46:14 +0530

    chore: upgrade Next.js from 14.2.15 to 15.2.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased mx-auto max-w-7xl`}
+          suppressHydrationWarning
         >
           <Script
             defer
